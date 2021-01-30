165de156cb835708137386bae38f049b2573289d
leejongwook1 <leejongwook@naver.com>
2021-01-30 15:24:52 +0900

    useing firebase auth

---
+++ src/fbase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+//깃허브 만큼은 보이게 하지 않으려고 환경변수 사용
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
